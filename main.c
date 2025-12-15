d60422214912a8d1413f8e5d476a42f933be4998
Simone Gentili <gensimone.git@gmail.com>
2025-12-15 20:27:13 +0100

    Add more options and checks.

--- main.c
+++ main.c
@@ -3,34 +3,50 @@
 #include <errno.h>
 #include <getopt.h>
 
+#define VERSION "0.0.1"
+
+/* Format default values. */
+#define LINES 20
+#define COLUMNS 3
+#define WIDHT 21
+#define GAP 6
+#define MPROC 0
 
 struct option longopts[] = {
-        { "verbose", no_argument,       NULL, 'v' },
-        { "help",    no_argument,       NULL, 'h' },
-        { "debug",   no_argument,       NULL, 'd' },
-        { "input",   required_argument, NULL, 'i' },
-        { "output",  required_argument, NULL, 'o' },
-        { NULL,      0,                 NULL,  0 },
+        { "verbose",      no_argument,       NULL, 'v' },
+        { "help",         no_argument,       NULL, 'h' },
+        { "debug",        no_argument,       NULL, 'd' },
+        { "input",        required_argument, NULL, 'i' },
+        { "output",       required_argument, NULL, 'o' },
+        { "lines",        required_argument, NULL, 'l' },
+        { "columns",      required_argument, NULL, 'c' },
+        { "width",        required_argument, NULL, 'w' },
+        { "gap",          required_argument, NULL, 'g' },
+        { "multiprocess", required_argument, NULL, 'm' },
+        { NULL,           0,                 NULL,  0  },
 };
 
 void help() 
 {
-        // TODO: make dinamic version
-        puts("textf (0.0.1)");
-        // Iterate over longopts and generate the Help message.
-        for (int i = 0; i < sizeof(longopts)/sizeof(longopts[0]); i++) {
+        printf("textf (%s)\n", VERSION);
+        for (int i = 0; i < sizeof(longopts)/sizeof(longopts[0]) - 1; i++) {
                 printf("--%s -%c\n", longopts[i].name, longopts[i].val);
         }
 }
 
 int main(int argc, char *argv[]) 
 {
+        int verbose = 0;
+        int debug = 0;
         char *input = NULL;
         char *output = NULL;
-        int debug = 0;
-        int verbose = 0;
-        int opt;
+        int lines = LINES;
+        int columns = COLUMNS;
+        int width = WIDHT;
+        int gap = GAP;
+        int mproc = MPROC;
 
+        int opt;
         while ((opt = getopt_long(argc, argv, "vhi:o:", longopts, NULL)) != -1) {
                 switch (opt) {
                         case 'v':
@@ -39,37 +55,71 @@ int main(int argc, char *argv[])
                         case 'h':
                                 help();
                                 return EXIT_SUCCESS;
+                        case 'd':
+                                debug = 1;
+                                break;
                         case 'i':
                                 input = optarg;
                                 break;
                         case 'o':
                                 output = optarg;
                                 break;
-                        case 'd':
-                                debug = 1;
+                        case 'l':
+                                lines = atoi(optarg);
+                                break;
+                        case 'c':
+                                columns = atoi(optarg);
+                                break;
+                        case 'w':
+                                width = atoi(optarg);
+                                break;
+                        case 'g':
+                                gap = atoi(optarg);
+                                break;
+                        case 'm':
+                                mproc = 1;
                                 break;
                         case '?':
                                 return EXIT_FAILURE;
                 }
         }
+        
+        /* Check format options. */
+
+        if (!lines)   fputs("lines cannot be zero.",   stderr);
+        if (!columns) fputs("columns cannot be zero.", stderr);
+        if (!width)   fputs("width cannot be zero.",   stderr);
+
+        if (!lines || !columns || !width) return EXIT_FAILURE;
+
+        /* Check IO files. */
 
         if (input == NULL || output == NULL) {
-                dprintf(2, "Missing required option.\n");
+                fprintf(stderr, "Missing --input or --output options.\n");
                 return EXIT_FAILURE;
         }
 
         FILE *finput =  fopen(input,  "r");
-        if (finput == NULL) {
-                dprintf(2, "Cannot open file: %s\n", input);
-                dprintf(2, "Errno: %d\n", errno);
+        if (!finput) {
+                perror("fopen");
                 return EXIT_FAILURE;
         }
 
         FILE *foutput = fopen(output, "w");
-        if (foutput == NULL) {
-                dprintf(2, "Cannot open file: %s\n", input);
-                dprintf(2, "Errno: %d\n", errno);
+        if (!foutput) {
+                perror("fopen");
                 return EXIT_FAILURE;
+        }
+
+        if (debug) {
+                printf("ftext (%s)\n", VERSION);
+                printf("input:   %s\n", input);
+                printf("output:  %s\n", output);
+                printf("lines:   %d\n", lines);
+                printf("columns: %d\n", columns);
+                printf("width:   %d\n", width);
+                printf("gap:     %d\n", gap);
+                printf("mproc:   %d\n", mproc);
         }
 
         fclose(finput);
@@ -77,4 +127,3 @@ int main(int argc, char *argv[])
 
         return 0;
 }
-
