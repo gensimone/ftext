1fb54760ed3ee9c9840064509deb168a2a06a5de
Simone Gentili <gensimone.git@gmail.com>
2025-12-16 15:12:11 +0100

    Remove --input and --output flags.
    The program will read from stdin and write to stdout.

--- main.c
+++ main.c
@@ -4,25 +4,19 @@
 #include <getopt.h>
 
 #define VERSION "0.0.1"
-
-/* Format default values. */
+#define VERBOSE 0
 #define LINES 20
 #define COLUMNS 3
-#define WIDHT 21
+#define WIDTH 21
 #define GAP 6
-#define MPROC 0
 
 struct option longopts[] = {
         { "verbose",      no_argument,       NULL, 'v' },
         { "help",         no_argument,       NULL, 'h' },
-        { "debug",        no_argument,       NULL, 'd' },
-        { "input",        required_argument, NULL, 'i' },
-        { "output",       required_argument, NULL, 'o' },
         { "lines",        required_argument, NULL, 'l' },
         { "columns",      required_argument, NULL, 'c' },
         { "width",        required_argument, NULL, 'w' },
         { "gap",          required_argument, NULL, 'g' },
-        { "multiprocess", required_argument, NULL, 'm' },
         { NULL,           0,                 NULL,  0  },
 };
 
@@ -36,18 +30,14 @@ void help()
 
 int main(int argc, char* argv[])
 {
-        int verbose = 0;
-        int debug = 0;
-        char* input = NULL;
-        char* output = NULL;
-        int lines = LINES;
+        int verbose = VERBOSE;
+        int lines   = LINES;
         int columns = COLUMNS;
-        int width = WIDHT;
-        int gap = GAP;
-        int mproc = MPROC;
+        int width   = WIDTH;
+        int gap     = GAP;
 
         int opt;
-        while ((opt = getopt_long(argc, argv, "vhi:o:", longopts, NULL)) != -1) {
+        while ((opt = getopt_long(argc, argv, "l:c:w:g:vh", longopts, NULL)) != -1) {
                 switch (opt) {
                         case 'v':
                                 verbose = 1;
@@ -55,15 +45,6 @@ int main(int argc, char* argv[])
                         case 'h':
                                 help();
                                 return EXIT_SUCCESS;
-                        case 'd':
-                                debug = 1;
-                                break;
-                        case 'i':
-                                input = optarg;
-                                break;
-                        case 'o':
-                                output = optarg;
-                                break;
                         case 'l':
                                 lines = atoi(optarg);
                                 break;
@@ -76,50 +57,16 @@ int main(int argc, char* argv[])
                         case 'g':
                                 gap = atoi(optarg);
                                 break;
-                        case 'm':
-                                mproc = 1;
-                                break;
                         case '?':
                                 return EXIT_FAILURE;
                 }
         }
         
-        /* Check format options. */
+        /* Check options integrity. */
         if (!lines)   fputs("Argument of --lines must be greater than zero.\n", stderr);
-        if (!width)   fputs("Argument of --widht must be greater than zero.\n", stderr);
-        if (!columns) fputs("Argument of --cols must be greater than zero.\n", stderr);
+        if (!width)   fputs("Argument of --width must be greater than zero.\n", stderr);
+        if (!columns) fputs("Argument of --columns must be greater than zero.\n", stderr);
         if (!lines || !columns || !width) return EXIT_FAILURE;
-
-        /* Check IO */
-        if (!input) fputs("Argument of --input is required.\n", stderr);
-        if (!input) fputs("Argument of --output is required.\n", stderr);
-        if (!input || !output) return EXIT_FAILURE;
-
-        FILE* finput =  fopen(input,  "r");
-        if (!finput) {
-                perror("fopen");
-                return EXIT_FAILURE;
-        }
-
-        FILE* foutput = fopen(output, "w");
-        if (!foutput) {
-                perror("fopen");
-                return EXIT_FAILURE;
-        }
-
-        if (debug) {
-                printf("ftext (%s)\n", VERSION);
-                printf("input:   %s\n", input);
-                printf("output:  %s\n", output);
-                printf("lines:   %d\n", lines);
-                printf("columns: %d\n", columns);
-                printf("width:   %d\n", width);
-                printf("gap:     %d\n", gap);
-                printf("mproc:   %d\n", mproc);
-        }
-
-        fclose(finput);
-        fclose(foutput);
 
         return 0;
 }
