63ebb72c39f7ac9e08ed9e1240d23637437649cc
Simone Gentili <gensimone.git@gmail.com>
2026-01-04 15:41:19 +0100

    Update test.sh
    For some reasong $(../ftext ... doens't seem to work.

--- tests/test.sh
+++ tests/test.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-res=$(../ftext < ./input.txt)
+../ftext -c 3 -w 21 -g 6 -l 24 <./input.txt >provided.txt
 
 if [ $? -gt 0 ]; then
-    echo "ftext: error." >&2
-    echo "Test aborted." >&2
-    exit 1
+	echo "ftext: error." >&2
+	echo "Test aborted." >&2
+	exit 1
 fi
 
+cat ./provided.txt
+
 expected_words=$(cat ./expected.txt | wc -w)
-received_words=$(echo $res | wc -w)
+received_words=$(cat ./provided.txt | wc -w)
 
 if ! [ $received_words -eq $expected_words ]; then
-    echo "Test failed"
-    echo ":: expected: $expected_words"
-    echo ":: received: $received_words"
+	echo "Test failed"
+	echo ":: expected: $expected_words"
+	echo ":: received: $received_words"
 else
-    echo "Test passed"
+	echo "Test passed"
+fi
 
 exit 0
