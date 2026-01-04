bb2170ba194d2661e18e602e01db3a71ae7cd3df
Simone Gentili <gensimone.git@gmail.com>
2026-01-04 21:50:12 +0100

    Update ftext.c format style.

--- ftext.c
+++ ftext.c
@@ -29,75 +29,79 @@ static struct option const longopts[] = {
 
 void usage(void)
 {
-    printf("Usage %s [OPTIONS]\n", PROGRAM_NAME);
-    puts("Column the text provided in stdin and print it in stdout.");
-    puts("The following options can be used to customize the formatting.");
-    printf("  --columns -c    number of columns (default %d)\n", COLS);
-    printf("  --lines   -l    number of lines (default %d)\n", LINES);
-    printf("  --width   -w    number of characters on a row of a column "
-           "(default "
-           "%d)\n",
-           WIDTH);
-    printf("  --gap     -l    number of spaces between columns (default %d)\n", GAP);
-    puts("  --help    -h    display this help and exit");
-    puts("  --version -v    output version information and exit");
-    exit(EXIT_SUCCESS);
+  printf("Usage %s [OPTIONS]\n", PROGRAM_NAME);
+  puts("Column the text provided in stdin and print it in stdout.");
+  puts("The following options can be used to customize the formatting.");
+  printf("  --columns -c    number of columns (default %d)\n", COLS);
+  printf("  --lines   -l    number of lines (default %d)\n", LINES);
+  printf("  --width   -w    number of characters on a row of a column "
+         "(default "
+         "%d)\n",
+         WIDTH);
+  printf("  --gap     -l    number of spaces between columns (default %d)\n",
+         GAP);
+  puts("  --help    -h    display this help and exit");
+  puts("  --version -v    output version information and exit");
+  exit(EXIT_SUCCESS);
 }
 
 void emit_version(void)
 {
-    printf("%s\n", VERSION);
-    exit(EXIT_SUCCESS);
+  printf("%s\n", VERSION);
+  exit(EXIT_SUCCESS);
 }
 
 void emit_invalid_arg(char* opt)
 {
-    fprintf(stderr, "Invalid argument for option %s\n", opt);
-    emit_try_help();
+  fprintf(stderr, "Invalid argument for option %s\n", opt);
+  emit_try_help();
 }
 
 void emit_try_help(void)
 {
-    fprintf(stderr, "Try '%s --help' for more information.\n", PROGRAM_NAME);
-    exit(EXIT_FAILURE);
+  fprintf(stderr, "Try '%s --help' for more information.\n", PROGRAM_NAME);
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char* argv[])
 {
-    int cols = COLS;
-    int lines = LINES;
-    int width = WIDTH;
-    int gap = GAP;
+  int cols = COLS;
+  int lines = LINES;
+  int width = WIDTH;
+  int gap = GAP;
 
-    int opt;
-    // FIXME: use strol instead of atoi
-    while ((opt = getopt_long(argc, argv, "c:l:w:g:vhd", longopts, NULL)) != -1)
-        switch (opt) {
-        case 'v':
-            emit_version();
-        case 'h':
-            usage();
-        case 'c':
-            cols = strtol(optarg, NULL, 10);
-            break;
-        case 'l':
-            lines = atoi(optarg);
-            break;
-        case 'w':
-            width = atoi(optarg);
-            break;
-        case 'g':
-            gap = atoi(optarg);
-            break;
-        case '?':
-            emit_try_help();
-        }
-    if (cols <= 0)
-        emit_invalid_arg("--columns");
-    if (lines <= 0)
-        emit_invalid_arg("--lines");
-    if (width <= 0)
-        emit_invalid_arg("--width");
+  int opt;
+  // FIXME: use strol instead of atoi
+  while ((opt = getopt_long(argc, argv, "c:l:w:g:vhd", longopts, NULL)) != -1)
+    switch (opt) {
+    case 'v':
+      emit_version();
+      break;
+    case 'h':
+      usage();
+      break;
+    case 'c':
+      cols = strtol(optarg, NULL, 10);
+      break;
+    case 'l':
+      lines = atoi(optarg);
+      break;
+    case 'w':
+      width = atoi(optarg);
+      break;
+    case 'g':
+      gap = atoi(optarg);
+      break;
+    case '?':
+      emit_try_help();
+      break;
+    }
+  if (cols <= 0)
+    emit_invalid_arg("--columns");
+  if (lines <= 0)
+    emit_invalid_arg("--lines");
+  if (width <= 0)
+    emit_invalid_arg("--width");
 
-    ftext(cols, lines, width, gap);
+  ftext(cols, lines, width, gap);
 }
