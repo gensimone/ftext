bda8cbb7323058cedcedc9c00cf91a51ffae6617
Simone Gentili <gensimone.git@gmail.com>
2026-01-04 21:48:06 +0100

    Update core.c and core.h

--- core.c
+++ core.c
@@ -8,135 +8,184 @@
 #define NEW_PAGE "\n %%%\n\n"
 
 char* next_word(void);
-int fpage(char** page, int cols, int lines, int width, int gap);
-void ftext(int cols, int lines, int width, int gap);
+void ftext(int cols, const int lines, const int width, const int gap);
 
 /*
   Read from stdin and return the first encountered word.
   Return null character if, before founding any non-space
   character, a \n is encountered. This is useful as a way
-  to recognized new lines without polluting the words.
+  to recognized new lines without polluting the words with
+  escape sequences.
  */
 char* next_word(void)
 {
-    int c;
-    size_t len = 0;
-    size_t cap = 100; // more than enough.
-    char* buf = die_on_fail_calloc(cap, sizeof(char));
+  int c;
+  size_t len = 0;
+  size_t cap = 100; // more than enough.
+  char* buf = die_on_fail_calloc(cap, sizeof(char));
 
-    // skip all escape sequences except \n.
-    while ((c = getchar()) != EOF && isspace(c)) {
-        if (c == '\n') {
-            buf[0] = '\0';
-            return buf;
-        }
+  // skip all escape sequences except \n.
+  while ((c = getchar()) != EOF && isspace(c)) {
+    if (c == '\n') {
+      buf[0] = '\0';
+      return buf;
     }
+  }
 
-    if (c == EOF) {
-        free(buf);
-        return NULL;
-    }
+  if (c == EOF) {
+    free(buf);
+    return NULL;
+  }
 
-    while (c != EOF && !isspace(c)) {
-        if (len + 1 >= cap) {
-            cap *= 2; // it may be overkill.
-            char* tmp = realloc(buf, cap);
-            if (!tmp) {
-                free(buf);
-                return NULL;
-            }
-            buf = tmp;
-        }
+  while (c != EOF && !isspace(c)) {
+    // capacity exceeded, need realloc.
+    if (len + 1 >= cap)
+      buf = die_on_fail_realloc(buf, cap *= 2);
 
-        buf[len++] = (char) c;
-        c = getchar();
-    }
-    buf[len] = '\0';
-    return buf;
+    buf[len++] = (char) c;
+    c = getchar();
+  }
+  buf[len] = '\0';
+  return buf;
 }
 
-void ftext(int cols, int lines, int width, int gap)
+/* Basically the entry point.
+   Based on the rules described in project.pdf, format the text from stdin
+   and provide the formatted text in stdout.
+ */
+void ftext(const int cols, const int lines, const int width, const int gap)
 {
-    size_t page_width = (width * cols + (cols - 1) * gap) * 4;
-    for (;;) {
-        char** page = die_on_fail_palloc(lines, page_width);
-        int status = fpage(page, cols, lines, width, gap);
-        for (int y = 0; y < lines; y++)
-            printf("%s\n", page[y]);
-        free_palloc(page, lines);
-        if (status == EOF)
+  size_t page_width = (width * cols + (cols - 1) * gap) * 4;
+
+  // save loaded words across pages to avoid missing
+  // words when moving onto next page.
+  int cap = 100; // capacity of the 'words' array
+  int cw = 0;    // current width
+  int wc = 0;    // word counter
+  int eof = 0;   // notify EOF
+  char** words = (char**) die_on_fail_calloc(cap, sizeof(char*));
+
+  for (;;) {
+    char** page = die_on_fail_palloc(lines, page_width);
+
+    for (int c = 0; c < cols; c++) {    // Format individual page.
+      for (int l = 0; l < lines; l++) { // Format an entire column.
+        while (1) {                     // Format a line of a column.
+          char* w = next_word();
+
+          // EOF reached, flush any pending word and quit.
+          if (w == NULL) {
+            if (wc > 0) {
+              char* line = die_on_fail_calloc(width, sizeof(char));
+              // Insert gap before actual text and in columns that are
+              // not the first. This way we prevent trailing whitespaces.
+              if (c > 0)
+                strcat(page[l], strspace(gap));
+
+              // Concatenate the actual text.
+              str_left_align(words, line, wc);
+              strcat(page[l], line);
+
+              free(line);
+            }
+
+            free(w);
+            eof = 1;
+            goto print_page;
+          }
+
+          // I think I need to calculate this because
+          // otherwise the text is not align the way it should when
+          // non ASCII characters are found inside the word.
+          int wl = strlen(w) - count_notasciic(w);
+
+          // New line means go to the next paragraph.
+          if (wl == 0) {
+            next_word(); // This will skip the empty line.
+            char* line = die_on_fail_calloc(width, sizeof(char));
+            str_left_align(words, line, wc);
+            // Insert gap before actual text and in columns that are
+            // not the first. This way we prevent trailing whitespaces.
+            if (c > 0)
+              strcat(page[l], strspace(gap));
+
+            strcat(page[l], line);
+
+            // Do not add whitespaces on the left side if we are
+            // at the left most column.
+            if (c < cols - 1)
+              strcat(page[l], strspace(width - cw - wc + 1));
+
+            // If we are at the bottom of the page or at
+            // the left most column, do not insert a blank line.
+            if (l < lines - 1 && c < cols - 1)
+              strcat(page[l + 1], strspace(width));
+
+            cw = 0;
+            wc = 0;
+
+            free(line);
+
+            // Paragraphs are separated by a blank line so
+            // go two lines forward.
+            l++;
             break;
-        printf("%s", NEW_PAGE);
-    }
-}
+          }
 
-int fpage(char** page, int cols, int lines, int width, int gap)
-{
-    int max_words = 100;
-    char** words = (char**) die_on_fail_calloc(max_words, sizeof(char*));
-    unsigned int current_width = 0;
-    int wc = 0;
-    for (int c = 0; c < cols; c++) {
-        for (int l = 0; l < lines; l++) {
-            while (1) {
-                char* w = next_word();
+          // Buffer exceeded, flush loaded words.
+          else if (wl + cw + wc > width) {
+            char* line = die_on_fail_calloc(width, sizeof(char));
 
-                // EOF Reached.
-                if (w == NULL) {
-                    if (wc > 0) {
-                        char* line = die_on_fail_calloc(width, sizeof(char));
-                        // alignment
-                        str_left_align(words, line, wc);
-                        strcat(page[l], line);
-                        free(line);
-                    }
-                    free(w);
-                    free_palloc(words, max_words);
-                    return EOF;
-                }
+            // Insert gap before actual text and in columns that are
+            // not the first. This way we prevent trailing whitespaces.
+            if (c > 0)
+              strcat(page[l], strspace(gap));
 
-                int wl = strlen(w) - count_notascii(w);
-                // New line found.
-                if (wl == 0) {
-                    next_word();
-                    char* line = die_on_fail_calloc(width, sizeof(char));
-                    str_left_align(words, line, wc);
-                    strcat(page[l], line);
-                    strcat(page[l], strspace(gap + width - current_width - wc + 1));
-                    if (l < lines - 1)
-                        strcat(page[l + 1], strspace(width + gap));
-                    current_width = 0;
-                    wc = 0;
-                    words[0] = NULL;
-                    l++;
-                    free(line);
-                    break;
-                }
-                // Buffer has no space for 'word'.
-                else if (wl + current_width + wc > width) {
-                    char* line = die_on_fail_calloc(width, sizeof(char));
-                    str_border_align(words, line, wc, width, current_width);
-                    strcat(page[l], line);
-                    strcat(page[l], strspace(gap));
-                    current_width = wl;
-                    wc = 1;
-                    words[0] = w;
-                    free(line);
-                    break;
-                }
-                // Buffer has enough space for 'word'.
-                else {
-                    if (wc >= max_words) {
-                        max_words += 100;
-                        words = (char**) die_on_fail_realloc(words, max_words * sizeof(char*));
-                    }
-                    words[wc] = w;
-                    wc++;
-                    current_width += wl;
-                }
-            }
+            // Concatenate the actual text.
+            str_border_align(words, line, wc, width, cw);
+            strcat(page[l], line);
+
+            // We need to fill width - len(word) with spaces when
+            // only one word is going to be placed.
+            if (wc == 1)
+              strcat(page[l], strspace(width - strlen(words[0])));
+
+            // Remember about the last word.
+            cw = wl;
+            wc = 1;
+            words[0] = w;
+
+            free(line);
+            break; // go to next line.
+          }
+
+          else {
+            // Capacity exceeded.
+            if (wc >= cap)
+              words = (char**) die_on_fail_realloc(words,
+                                                   (cap *= 2) * sizeof(char*));
+
+            // Save the word and move on until
+            // we reach EOF or exceed the buffer.
+            words[wc] = w;
+            wc++;
+            cw += wl;
+          }
         }
+      }
     }
-    free_palloc(words, lines);
-    return 0;
+
+  print_page:
+    // Flush the entire page to stdout.
+    for (int y = 0; y < lines; y++)
+      printf("%s\n", page[y]);
+    free_palloc(page, lines);
+
+    if (eof)
+      break;
+
+    printf("%s", NEW_PAGE);
+  }
+
+  free_palloc(words, cap);
 }
--- core.h
+++ core.h
@@ -1,4 +1,3 @@
-
 char* next_word(void);
 int fpage(char** page, int cols, int lines, int width, int gap);
 void ftext(int cols, int lines, int width, int gap);
