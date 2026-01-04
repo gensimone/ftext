7d17540c4e8629e18d9e01d3a35b6a9aad7afb77
Simone Gentili <gensimone.git@gmail.com>
2026-01-04 17:21:55 +0100

    Refactor test.sh. Use diff instead of wc.
    The all wc thing was a mistake. It doens't tell you
    whether the two files are identical or not in terms of content.
    Diff in the other hand tells you that.

--- tests/test.sh
+++ tests/test.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
-../ftext -c 3 -w 21 -g 6 -l 24 <./input.txt >provided.txt
+../ftext -c 3 -w 21 -g 6 -l 24 <./input.txt >./provided.txt
 
 if [ $? -gt 0 ]; then
-	echo "ftext: error." >&2
-	echo "Test aborted." >&2
-	exit 1
+    echo "Test aborted.."
+    exit 1
 fi
 
-cat ./provided.txt
-
-expected_words=$(cat ./expected.txt | wc -w)
-received_words=$(cat ./provided.txt | wc -w)
-
-if ! [ $received_words -eq $expected_words ]; then
-	echo "Test failed"
-	echo ":: expected: $expected_words"
-	echo ":: received: $received_words"
+if diff -b ./expected.txt ./provided.txt; then
+    echo "Test passed."
 else
-	echo "Test passed"
+    echo "Test failed."
 fi
-
-exit 0
